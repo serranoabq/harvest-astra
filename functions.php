eb0a8774c7d948c688e11fde38539f5b3da982b7
serranoabq <serranoabq@gmail.com>
2020-12-12 21:54:33 -0700

    First commit

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Harvest Astra Theme functions and definitions
+ *
+ * @package Harvest Astra
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_HARVEST_ASTRA_VERSION', '1.0.0' );
+
+require_once get_template_directory() . '/framework/framework.php'; 
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'harvest-astra-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_HARVEST_ASTRA_VERSION, 'all' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
